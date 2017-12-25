27b8ae054e8de5a30e0cdb5878838e13f65be547
sriramprakhya <sriramscience7@gmail.com>
2017-12-25 21:15:25 +0530

    ham : gzosp_utils
    special thanx to :@Martinusbe

--- extract-files.sh
+++ extract-files.sh
@@ -27,9 +27,9 @@ VENDOR=zuk
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+GZOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$GZOSP_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -53,7 +53,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$GZOSP_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
