35c3c7472953cb89e99016ce327fce6d221883f4
sriramprakhya <sriramscience7@gmail.com>
2017-12-25 21:24:45 +0530

    ham :gzosp-ify

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-VNDK_SP_LIBRARIES := \
-    android.hardware.graphics.allocator@2.0 \
-    android.hardware.graphics.mapper@2.0 \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.common@1.0 \
-    android.hardware.renderscript@1.0 \
-    android.hardware.memtrack@1.0-impl \
-    android.hidl.memory@1.0 \
-    libRSCpuRef \
-    libRSDriver \
-    libRS_internal \
-    libbacktrace \
-    libbase \
-    libbcinfo \
-    libblas \
-    libc++ \
-    libcompiler_rt \
-    libcutils \
-    libft2 \
-    libhardware \
-    libhidlbase \
-    libhidlmemory \
-    libhidltransport \
-    libhwbinder \
-    libion \
-    liblzma \
-    libpng \
-    libunwind \
-    libutils
-=======
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.vndk.version=26.1.0 \
 PRODUCT_PACKAGES += \
@@ -57,4 +25,3 @@ PRODUCT_PACKAGES += \
     libunwind.vndk-sp\
     liblzma.vndk-sp\
 
->>>>>>> 6be934b... ham : update vndk for lineage-15.1 or 8.1 trees
