76e3f6f1e6478b667e4f05000203d41457d75f56
sriramprakhya <sriramscience7@gmail.com>
2017-12-26 09:38:49 +0530

    ham : add camera wrappers this is test

--- ham.mk
+++ ham.mk
@@ -99,10 +99,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy \
     camera.msm8974 \
-    Snap
+    SnapdragonCamera
 
 # Charger
 PRODUCT_PACKAGES += \
@@ -370,7 +368,7 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalv
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # MSM8974 Headers
-PRODUCT_KERNEL_HEADERS := device/zuk/ham/kernel-headers
+#PRODUCT_KERNEL_HEADERS := device/zuk/ham/kernel-headers
 
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/zuk/ham/ham-vendor.mk)
